f8c83ad61e439b50aa09c98fc937dc06ef7e3656
Felipe Faria <felipevisu@gmail.com>
2021-03-25 13:50:10 -0300

    karalho

--- pages/produtos.js
+++ pages/produtos.js
@@ -15,7 +15,7 @@ export default function Home({ response }) {
     channel: "casa-nature",
     sort: {
       field: "NAME",
-      direction: "DESC"
+      direction: "ASC"
     },
     filter: {
       isPublished: true,
@@ -23,15 +23,11 @@ export default function Home({ response }) {
     }
   }
 
-  const { data, error } = useSWR(variables, variables => getAllProducts(variables)); 
+  const { data, error } = useSWR([variables], (variables) => getAllProducts(variables));
 
   useEffect(() => {
     console.log(data)
   }, [data])
-
-  if(!data){
-    return <>loading</>
-  }
 
   return (
     <div>
@@ -45,9 +41,7 @@ export default function Home({ response }) {
           <h3 className="text-xl font-light">Produtos</h3>
         </div>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 xl:gap-6">
-          {data.products.map((product, key) => 
-            <ProductCard key={key} {...product} />
-          )}
+          
         </div>
       </div>
       
@@ -56,22 +50,11 @@ export default function Home({ response }) {
 }
 
 export async function getStaticProps(context) {
-  const response = await getAllProducts({
-    first: 10,
-    channel: "casa-nature",
-    sort: {
-      field: "NAME",
-      direction: "ASC"
-    },
-    filter: {
-      isPublished: true,
-      channel: "casa-nature"
-    }
-  })
+  
 
   return {
     props: {
-      response,
+      response: [],
     },
     revalidate: 10000
   }
