dd67cf78d0c3bd87d4643031ed16563f0d6a5846
Felipe Faria <felipevisu@gmail.com>
2021-04-08 18:57:12 -0300

    adf

--- components/checkout/Payment/Pagarme/utils.js
+++ components/checkout/Payment/Pagarme/utils.js
@@ -14,6 +14,6 @@ export async function validatePagarme(data){
 
 	var cardValidations = pagarme.validate({ card: cardData })
 	cardValidations.card.document = validateCPF(document) || validateCNPJ(document) 
-
+	
 	return cardValidations.card
 }
