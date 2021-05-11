8b9744fe0bb4d669745026e89393c56489a76a79
Felipe Faria <felipevisu@gmail.com>
2021-05-11 17:04:52 -0300

    manigest

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 const { withSentryConfig } = require('@sentry/nextjs');
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 const moduleExports = withPWA({
   images: {
@@ -12,21 +8,12 @@ const moduleExports = withPWA({
     path: '/_next/image',
   },
   pwa: {
-    dest: 'public'
+    dest: 'public',
+    runtimeCaching,
   },
   productionBrowserSourceMaps: true,
 });
 
-const SentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
+const SentryWebpackPluginOptions = {};
 
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
 module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
