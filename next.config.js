e77cff00d858ce64fa2f3649ddc1533dc681db5c
Felipe Faria <felipevisu@gmail.com>
2021-05-21 14:58:15 -0300

    next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 const { withSentryConfig } = require('@sentry/nextjs');
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
-const moduleExports = {
+const moduleExports = withPWA({
   images: {
     domains: ['loja-saudavel.s3.amazonaws.com'],
     path: '/_next/image',
   },
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
   productionBrowserSourceMaps: true,
-};
+});
 
 const SentryWebpackPluginOptions = {};
 
