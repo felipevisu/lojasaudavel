a23b4bd1d174b8b35004eb829ea476b2d0251d70
Felipe Faria <felipevisu@gmail.com>
2021-06-07 15:32:12 -0300

    fix payment ids

--- components/checkout/Payment/Payment.js
+++ components/checkout/Payment/Payment.js
@@ -43,13 +43,13 @@ export function Payment(props){
           <Voucher />
           <span className="font-semibold mb-2 text-gray-500 block">Forma de pagamento</span>
           <div className="border-b pb-3 mb-3 lg:flex">
-            {lojista &&
+            {pagarme &&
               <label className="flex items-center mr-3 py-1">
                 <input className="text-green-500 mr-1" type="radio" checked={selected === "pagarme"} name="place" value="pagarme" onChange={handleChange} />
                 <span>Pagar agora no site</span>
               </label>
             }
-            {pagarme &&
+            {lojista &&
               <label className="flex items-center py-1">
                 <input className="text-green-500 mr-1" type="radio" checked={selected === "lojista"} name="place" value="lojista" onChange={handleChange} />
                 <span>Pagar na entrega/retirada</span>
@@ -59,14 +59,14 @@ export function Payment(props){
         </>
       }
 
-      {lojista &&
-        <div className={`${selected === 'lojista' ? 'block' : 'hidden'}`}>
-          <Lojista topLoading={loading} setTopLoading={setLoading} />
-        </div>
-      }
       {pagarme &&
         <div className={`${selected === 'pagarme' ? 'block' : 'hidden'}`}>
           <Pagarme topLoading={loading} setTopLoading={setLoading} changeMethod={changeMethod} config={pagarme.config} />
+        </div>
+      }
+      {lojista &&
+        <div className={`${selected === 'lojista' ? 'block' : 'hidden'}`}>
+          <Lojista topLoading={loading} setTopLoading={setLoading} />
         </div>
       }
     </div>
