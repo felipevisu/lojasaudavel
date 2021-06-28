bfd21abbbc3cef7e6f37a5830f2dd2a59200c103
Felipe Faria <felipevisu@gmail.com>
2021-06-28 13:48:39 -0300

    document

--- pages/_document.js
+++ pages/_document.js
@@ -30,7 +30,7 @@ class MyDocument extends Document {
 
           <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16.png' />
-          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+          <link rel="apple-touch-icon" href="/apple-icon.png" />
           <link rel='shortcut icon' href='/favicon.ico' />
           <link rel='manifest' href='/manifest.json' />
 
