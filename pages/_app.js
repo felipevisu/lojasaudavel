d71cdee05704c12b54be5f6652b183bb155eb202
Felipe Faria <felipevisu@gmail.com>
2021-06-28 13:40:47 -0300

    service worker

--- pages/_app.js
+++ pages/_app.js
@@ -10,8 +10,23 @@ import { ToastContainer } from 'react-toastify';
 import { CommerceProvider } from '../framework'
 import { Layout } from '../components/common'
 import { useApollo } from "../lib/apolloClient";
+import { useEffect } from "react"
 
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
 
   const apolloClient = useApollo(pageProps.initialApolloState);
 
