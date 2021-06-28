28c166eda28424f6a9e8dfe5c5c248aa19a316de
Felipe Faria <felipevisu@gmail.com>
2021-06-28 11:19:04 -0300

    fallback

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@ const { withSentryConfig } = require('@sentry/nextjs');
 const withPWA = require('next-pwa')
 
 const moduleExports = withPWA({
+  future: { webpack5: true },
+  pwa: {
+    dest: 'public',
+    fallbacks: {
+      image: '/fallback.png',
+    }
+  },
   images: {
     domains: ['loja-saudavel.s3.amazonaws.com'],
     path: '/_next/image',
--- pages/_offline.js
+++ pages/_offline.js
@@ -1,12 +1,18 @@
+import Head from 'next/head'
 
-export function OfflinePage(props){
+export function OfflinePage(){
   return (
-    <div className="w-full">
-      <div className="py-14 lg:p-20 text-center">
-        <h4 className="text-2xl">Você está offline!</h4>
-        <div className="text-xl font-bold my-4">Conecte-se a internet para acessaar nossa loja!</div>
+    <>
+      <Head>
+        <title>Loja Saudável - Você está offline!</title>
+      </Head>
+      <div className="w-full">
+        <div className="py-14 lg:p-20 text-center">
+          <h4 className="text-2xl">Você está offline!</h4>
+          <div className="text-xl font-bold my-4">Conecte-se a internet para acessar nossa loja!</div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
